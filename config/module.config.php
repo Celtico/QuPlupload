a8da6e41a88ff5e7f24c935fb04feeb4e5f0400a
Cel Tico <cel@cenics.net>
2013-04-09 14:15:47 +0200

    Up

--- config/module.config.php
+++ config/module.config.php
@@ -112,7 +112,7 @@ return array(
             'UrlRemove'          => '/quplupload/remove',
             'UrlLoad'            => '/quplupload/load',
             'DirUpload'          => '/uploads/files/plupload',
-            'DirUploadAbsolute'  =>  dirname(dirname(dirname(__DIR__))) . '/public/uploads/files/plupload',
+            'DirUploadAbsolute'  =>  dirname(dirname(dirname(dirname(__DIR__))))  . '/public/uploads/files/plupload',
             'DirJs'              => 'js/plugins/plupload',
             'Resize'             => array('1200','900'), //$width, $height
             'ThumbResize'        => array(
